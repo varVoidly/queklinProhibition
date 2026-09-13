5ca52f7fe6f29d2e05be710dd5f1dfa355e90d85
varVoidly <varvoidly@gmail.com>
2026-09-13 12:14:20 -0500

    Archival Commit - v0.1.3 - functionClockIn In Progess

--- queklinProhibition.py
+++ queklinProhibition.py
@@ -14,8 +14,10 @@
 
 
 # --------------------- Imports ---------------------
-import os
 from pathlib import Path
+import os
+import sys
+import time
 
 # --------------------- Global Variables ---------------------
 
@@ -33,7 +35,11 @@ pathMasterFile = pathLocalDirectory + "\\pythonQueklinMasterFile.txt"
 # Stores the pathway of the master file - Used to quickly reference the Master file without having to repeatedly locate it using functionFileLocator.
 # Note 1 - Double backslashes
 
-# --------------------- Function Defining ---------------------
+statusClockedIn = False
+
+timeStart = None
+timeEnd = None
+# --------------------- General Function Defining ---------------------
 # Note 2 - Function purposes clarification
 
 # Ensure the master exists, and creates it if not.
@@ -53,7 +59,40 @@ def functionFileLocator():
 		print("DEBUG/SUCCESS: File Found. Continuing.")
 
 
+def functionClearTerminal():
+	os.system('cls' if os.name == 'nt' else 'clear') # Clear terminal
 
+
+
+
+# --------------------- Main Menu Function Defining ---------------------
+
+
+# ==== Function: Clock In ====
+
+def functionFormatTime(sec):
+  mins = sec // 60 # Minutes
+  sec = sec % 60 # Seconds leftover after converting to minutes
+  hours = mins // 60 # Hours
+  mins = mins % 60 # Minutes leftover after converting to hours
+  print("Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),sec))
+
+# Example code stolen from a website:
+	# input("Press Enter to start")
+	# start_time = time.time()
+	# input("Press Enter to stop")
+	# end_time = time.time()
+	# time_lapsed = end_time - start_time
+	# time_convert(time_lapsed)
+
+
+def functionClockIn():
+	# Begins clocked-in stopwatch
+	statusClockedIn == True
+	timeStart = time.time()
+
+	# Begins watchers to ensure queklin is prohibited
+	
 
 
 
@@ -72,4 +111,43 @@ print("SUCCESS: Confirmed, Let Us Begin %s" % varName)
 print("---")
 
 functionFileLocator()
+functionClearTerminal()
+
+statusMainMenu = True
+while(statusMainMenu):
+	print("Main Menu.")
+	print("0. End program.")
+	print("1. Clock In")
+	print("2. Clock Out")
+	print("3. Display Current Clock In Information")
+	print("4. Display Clockout Information")
+	print("5. To-Do Lists")
+
+	inputMainMenu = input("INFORM/INPUT: Enter Desired Options Number")
+	match inputMainMenu:
+		case "0":
+			functionClearTerminal()
+			if(statusClockedIn == True):
+				statusLocal = True
+				while(statusLocal):
+					inputExitProgramClockedIn = ("INPUT: You Are Currently Clocked In, Are You Sure You Would Like To Clock Out? Y/N")
+					if(inputExitProgramClockedIn.upper() == "Y"):
+						print("SUCCESS: Exiting Program")
+# CLOCK OUT NEEDS TO BE PUT HERE WHEN THE CLOCKOUT FUNCTION IS MADE!!!!!
+						sys.exit # Exits the program
+			else:
+				print("SUCCESS: Exiting Program. ")
+				sys.exit
+
+		case "1":
+			print("SUCCESS: Clocking In.")
+			print("INFORM: This Program Will Run In The Background And Can be Interacted With While Clocked In, However It Must Remain Open.")
+			input("INFORM: Press Enter To Continue")
+			functionClockIn()
+
+		case "2":
+
+
+
+
 
